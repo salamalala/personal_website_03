1d80f4575bd84a50e09222ced86b6a69ecb953c4
salamalala <hello@ninaregli.ch>
2017-10-19 11:49:44 +0200

    feat(intro): improving header design

--- assets/main.js
+++ assets/main.js
@@ -28,6 +28,81 @@ AnimationBackground.prototype = {
 
 var animationBackground = new AnimationBackground();
 
+var tl = new TimelineMax({repeat:-1}),
+    blockDuration = 0.4,
+    blackBlockDelay = "+=2",
+    orangeBlockDelay = "+=0.4",
+    orangeBlockMovingDelay = "+=0.3",
+    textDelay = "-=0.3",
+    textDuration = 0.8,
+    textEase = Expo.easeOut,
+    blockRevealEase = Power1.easeInOut,
+    blockDisappearsEase = Power1.easeOut
+    ;
+
+tl
+
+  /*First set*/
+   /* Text 1 appears */
+  .fromTo(".subtitle__1", textDuration, {opacity: 0, scale: 0.6, transformOrigin: "100% 0%"}, {opacity: 1, scale: 1, ease: textEase}, textDelay)
+
+  /*Black block appears */
+  .to(".intro__reveal-block__dark", blockDuration, { ease: blockRevealEase, left: 0}, blackBlockDelay)
+
+  /* Text 1 disappears */
+  .set(".subtitle__1", {opacity: 0})
+
+  /* Orange block appears */
+  .to(".intro__reveal-block__light", blockDuration, { ease: blockRevealEase, left: 0}, orangeBlockDelay)
+  //
+  /*Black block width shrinks down to 0px */
+  .set(".intro__reveal-block__dark", { left: '-100%'}, "+=0")
+  //
+  // /*Orange block disappears */
+  .to(".intro__reveal-block__light", blockDuration, {ease: blockDisappearsEase, xPercent: 110}, orangeBlockMovingDelay)
+
+  // /* Text 2 appears */
+  .fromTo(".subtitle__2", textDuration, {opacity: 0, scale: 0.6, transformOrigin: "100% 0%"}, {opacity: 1, scale: 1, ease: textEase}, textDelay)
+  //
+  // /*Black block appears */
+  .to(".intro__reveal-block__dark", blockDuration, { ease: blockRevealEase, left: 0}, blackBlockDelay)
+  //
+  .set(".subtitle__2", {opacity: 0})
+  //
+  // /*Orange block goes underneath black box */
+  .set(".intro__reveal-block__light", {zIndex: -1, left: '-100%', xPercent: 0})
+  //
+  // /*Orange block appears/
+  .to(".intro__reveal-block__light", blockDuration, { ease: blockRevealEase, left: 0, zIndex: 2}, orangeBlockDelay)
+  //
+  .set(".intro__reveal-block__dark", { left: '-100%'})
+
+  // /*Orange block disappears */
+  .to(".intro__reveal-block__light", blockDuration, {ease: blockDisappearsEase, xPercent: 110}, orangeBlockMovingDelay)
+  //
+  /* Text 3 appears */
+  .fromTo(".subtitle__3", textDuration, {opacity: 0,scale: 0.6, transformOrigin: "100% 0%"}, {opacity: 1, scale: 1, ease: textEase}, textDelay)
+  //
+  /*Black block appears */
+  .to(".intro__reveal-block__dark", blockDuration, { ease: blockRevealEase, left: 0}, blackBlockDelay)
+  //
+  .set(".subtitle__3",  {opacity: 0})
+  //
+  /*Orange block goes underneath black box */
+  .set(".intro__reveal-block__light", {zIndex: -1, left: '-100%', xPercent: 0})
+
+  // /*Orange block appears/
+  .to(".intro__reveal-block__light", blockDuration, { ease: blockRevealEase, left: 0, zIndex: 2}, orangeBlockDelay)
+
+  .set(".intro__reveal-block__dark", { width: 0})
+
+    // /*Orange block disappears */
+  .to(".intro__reveal-block__light", blockDuration, {ease: blockDisappearsEase, xPercent: 100}, orangeBlockMovingDelay);
+
+
+
+
+
 
 //js $(document).ready function in js
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -65,73 +140,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
       });
 
 
-      var tl = new TimelineMax({repeat:-1}),
-          blockDuration = 0.8,
-          blackBlockDelay = "+=0.4",
-          orangeBlockDelay = "+=0.3",
-          orangeBlockMovingDelay = "+=0.1",
-          textDuration = 0.4,
-          textEase = Expo.easeOut,
-          blockRevealEase = Power1.easeInOut,
-          blockDisappearsEase = Power1.easeOut
-          ;
 
-      tl
-
-        /*First set*/
-         /* Text 1 appears */
-        .fromTo(".subtitle__1", textDuration, {opacity: 0, scale: 0.5, transformOrigin: "100% 0%"}, {opacity: 1, scale: 1, ease: textEase}, "-=0.3")
-
-        /*Black block appears */
-        .to(".intro__reveal-block__dark", blockDuration, { ease: blockRevealEase, left: 0},"0")
-
-        /* Text 1 disappears */
-        .set(".subtitle__1", {opacity: 0})
-
-        /* Orange block appears */
-        .to(".intro__reveal-block__light", blockDuration, { ease: blockRevealEase, left: 0}, orangeBlockDelay)
-        //
-        /*Black block width shrinks down to 0px */
-        .set(".intro__reveal-block__dark", { left: '-100%'}, "+=0")
-        //
-        // /*Orange block disappears */
-        .to(".intro__reveal-block__light", blockDuration, {ease: blockDisappearsEase, xPercent: 110}, orangeBlockMovingDelay)
-
-        // /* Text 2 appears */
-        .fromTo(".subtitle__2", textDuration, {opacity: 0, scale: 0.5, transformOrigin: "100% 0%"}, {opacity: 1, scale: 1, ease: textEase}, "-=0.3")
-        //
-        // /*Black block appears */
-        .to(".intro__reveal-block__dark", blockDuration, { ease: blockRevealEase, left: 0}, blackBlockDelay)
-        //
-        .set(".subtitle__2", {opacity: 0})
-        //
-        // /*Orange block goes underneath black box */
-        .set(".intro__reveal-block__light", {zIndex: -1, left: '-100%', xPercent: 0})
-        //
-        // /*Orange block appears/
-        .to(".intro__reveal-block__light", blockDuration, { ease: blockRevealEase, left: 0, zIndex: 2}, orangeBlockDelay)
-        //
-        .set(".intro__reveal-block__dark", { left: '-100%'})
-
-        // /*Orange block disappears */
-        .to(".intro__reveal-block__light", blockDuration, {ease: blockDisappearsEase, xPercent: 110}, orangeBlockMovingDelay)
-        //
-        /* Text 3 appears */
-        .fromTo(".subtitle__3", textDuration, {opacity: 0,scale: 0.5, transformOrigin: "100% 0%"}, {opacity: 1, scale: 1, ease: textEase}, "-=0.3")
-        //
-        /*Black block appears */
-        .to(".intro__reveal-block__dark", blockDuration, { ease: blockRevealEase, left: 0}, blackBlockDelay)
-        //
-        .set(".subtitle__3",  {opacity: 0})
-        //
-        /*Orange block goes underneath black box */
-        .set(".intro__reveal-block__light", {zIndex: -1, left: '-100%', xPercent: 0})
-
-        // /*Orange block appears/
-        .to(".intro__reveal-block__light", blockDuration, { ease: blockRevealEase, left: 0, zIndex: 2}, orangeBlockDelay)
-
-        .set(".intro__reveal-block__dark", { width: 0})
-
-          // /*Orange block disappears */
-        .to(".intro__reveal-block__light", blockDuration, {ease: blockDisappearsEase, xPercent: 110}, orangeBlockMovingDelay);
 });
